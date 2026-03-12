f03722d0d10247b82da86dcf1ced78df4c0fd251
zomahoundorianne-cpu <zomahoundorianne@gmail.com>
2026-03-12 21:20:43 +0100

    fix port railway

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cron = require('node-cron');
 const { sendReminderEmails } = require('./emailService');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
